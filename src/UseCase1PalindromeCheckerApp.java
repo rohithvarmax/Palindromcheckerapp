f17a0656ee8e1294cba0dfc64c6b7675940373ad
Rohith Varma <101652646+rohithvarmax@users.noreply.github.com>
2026-02-23 10:06:22 +0530

    Update UseCase1PalindromeCheckerApp.java

--- src/UseCase1PalindromeCheckerApp.java
+++ src/UseCase1PalindromeCheckerApp.java
@@ -10,19 +10,14 @@ public class UseCase1PalindromeCheckerApp {
     public static void main(String[] args) {
 
         // Display Welcome Message
-        System.out.println("=====================================");
-        System.out.println("      Welcome to PalindromeChecker   ");
-        System.out.println("=====================================");
-
+        System.out.println("  Welcome to PalindromeChecker management system");
+     
         // Display Application Details
         System.out.println("Application Name : PalindromeChecker");
-        System.out.println("Version          : 1.0");
-        System.out.println("Author           : Your Name");
-        System.out.println("-------------------------------------");
-
-        // Application Flow Control
+        System.out.println("Version          : 1.0")
+         l
         System.out.println("Initializing application...");
-        System.out.println("Ready to check palindromes!");
+        System.out.println("system intialized succesfully");
 
         // Program can now continue to next use case (UC2)
     }
